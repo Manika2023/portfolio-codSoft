4f52ee2d688bd7da2878605615c5caa567d763d5
Manika2023 <136894527+Manika2023@users.noreply.github.com>
2023-09-12 20:26:46 +0530

    Add files via upload
    this is my portfolio

---
+++ index.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>manika portfolio</title>
+     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">    
+     <!-- font awesome cdn link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   
+<!-- !header section--  -->
+<header class="header">
+    <a href="" class="logo no-underline">portfolio</a>
+    <i class='bx bx-menu' id="menu-icon"></i>
+    <nav class="navbar">
+     <a href="#home" class="active">Home</a>
+     <a href="#about">About</a>
+     <a href="#services">Services</a>
+     <a href="#portfolio">portfolio</a>
+     <a href="#contact">contact</a>
+    </nav>
+</header>
+
+<!-- home-design -->
+<section class="home row" id="home">
+     <div class="home-content col-lg-4 col-md-4 col-12">
+          <h3>Hello, it's me</h3>
+          <h1>Manika Singh</h1>
+          <h3>And I'm a <span class="multiple-text"></span></h3>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id repudiandae doloremque nemo.</p>
+          <div class="social-media">
+               <a href=""><i class='bx bxl-facebook'></i></a>
+               <a href=""><i class='bx bxl-linkedin-square' ></i></a>
+               <a href=""><i class='bx bxl-github'></i></a>
+               <a href=""><i class='bx bxl-instagram-alt'></i></a>
+          </div>
+          <a href= href="" class="btn">download cv</a>
+     </div>
+     
+     <div class="home-img col-lg-4 col-md-4 col-12">
+          <img src="myphoto.png" alt="">
+     </div>
+</section>
+
+<!-- about-section start -->
+<h2 class="heading">About <span>me</span></h2>
+<section class="about row container-fluid" id="about">
+     <div class="about-img col-lg-4 col-md-4 col-12">
+          <img src="myphoto.png" alt="">
+     </div>
+     <div class="about-content col-lg-4 col-md-4 col-12 ">
+          <h3>Frontend Developer</h3>
+          <p >
+**Improve Your Online Presence with manika - Your Friendly Web Developer**<br>
+
+Welcome to the world of endless online possibilities! If you've been thinking about a website that not only looks great but also works smoothly, you're in the right place. I'm Manika, your dedicated web developer, and I'm here to turn your website dreams into reality.
+<br>
+**Why Choose Me:**<br>
+
+1. **Tailored Just for You:** Your website should be as unique as your business. I specialize in creating websites that match your brand, goals, and what your customers love.
+<br>
+2. **Easy for Everyone:** I make websites that are easy to use. No matter if you're on a computer or a phone, your website will work like a charm.
+<br>
+3. **Using the Latest Stuff:** I keep up with all the cool new things in web development. Your website will have all the latest features.
+<br>
+4. **Works Fast:** Slow websites can be frustrating. I make sure your website loads quickly so your visitors don't get bored waiting.
+<br>
+5. **Help Along the Way:** I don't just build your website and disappear. I'm here to help you with updates, fixes, and anything else you need.
+<br>
+**How I Work:**
+<br>
+1. **We Chat:** We'll start by talking about what you want. I want to understand your goals and ideas.
+<br>
+2. **Design:** I'll create a beautiful design for your website that matches your style and what your customers like.
+<br>
+3. **Building:** This is where the real magic happens. I'll turn the design into a working website that does everything you want.
+<br>
+4. **Testing:** I make sure everything works perfectly before your website goes live.
+<br>
+5. **Launch & Beyond:** Once your website is live, I'm here for the long haul. I'll keep your website running smoothly and help you make it even better.
+<br>
+**Let's Get Started:**
+<br>
+Ready to take your business or project online? Let's talk! Whether you have a big idea or just a small question, I'm here to help. Send me an email at manikak220@gmail.com or connect with me on [LinkedIn/whatsapp]. Let's make your website dreams come true!
+<br>
+---
+
+This simplified content maintains its friendly and inviting tone while making it easier for viewers to understand your services and how you can assist them with their website needs.</p>
+               <a href="#services" class="btn">read more</a>
+     </div>
+</section>
+
+<!-- services section start -->
+<section class="services" id="services">
+     <h2 class="heading"> our <span>services</span></h2>
+<div class="services-container row">
+<div class="services-box col-lg-4 col-md-4 col-12">
+     <i class="bx bx-code-alt"></i>
+     <h3>web development</h3>
+     <p>"Transforming visions into reality. We create bespoke websites that captivate, engage, and deliver results. From custom designs to seamless functionality, we've got you covered. Our services include web development, responsive des
+          ign, e-commerce solutions, and ongoing support. Let's craft your digital success story together."
+     </p>
+     <a href="#portfolio" class="btn">read more</a>
+</div>
+
+<div class="services-box col-lg-4 col-md-4 col-12">
+     <i class="bx bx-code-alt"></i>
+     <h3>video Editing</h3>
+     <p>"Elevate your visuals with our expert video editing services. We bring your footage to life with precision and creativity. Our services include seamless editing, color grading, motion graphics, and more. Whether it's a promotional video,
+           vlog, or special event, we'll make it shine. Let's tell your story through compelling visuals."
+     </p>
+     <a href="#portfolio" class="btn">read more</a>
+</div>
+<div class="services-box col-lg-4 col-md-4 col-12">
+     <i class="bx bx-code-alt"></i>
+     <h3>content writing</h3>
+     <p>"Unlock the power of words with our content writing services. We craft engaging, informative, and SEO-optimized content that resonates with your audience. From blog posts to website copy, our wordsmiths deliver compelling narratives tailored to your brand.
+           Elevate your online presence with our expertise in storytelling and digital content strategy."
+     </p>
+     <a href="#portfolio" class="btn">read more</a>
+</div>
+</div> 
+</section>
+
+<!--  portfolio section  -->
+<section class="portfolio" id="portfolio">
+<h2 class="heading">Latest <span>Projects</span></h2>
+
+<div class="portfolio-container">
+     <div class="portfolio-box">
+     <img src="amazon.png" alt="">
+          <div class="portfolio-layer">
+             <h4>web design</h4>
+             <p>this is clone of amazon website</p>
+         <a href="https://drive.google.com/file/d/1zWKmGesLGjaGyVkYBCGRptVALlFtSy_T/view?usp=drivesdk"><i class="bx bx-link-external"></i></a>
+          </div>
+     </div>
+
+     <div class="portfolio-box ">
+          <img src="s2.png" alt="">
+          <div class="portfolio-layer">
+             <h4>web design</h4>
+             <p>this is a cool website for a coaching center</p>
+         <a href="https://drive.google.com/file/d/1zXox-X6zPLJcgxtIQklnptHlSzrl_N2z/view?usp=drivesdk"><i class="bx bx-link-external"></i></a>
+          </div>
+     </div>
+
+     <div class="portfolio-box">
+          <img src="newswebsite.png" alt="">
+          <div class="portfolio-layer">
+             <h4>web design</h4>
+             <p>this is newswebsite </p>
+            <a href="https://drive.google.com/file/d/1znHoeqgvMNFH0k5Ob-yuCJaQA_kVCK1b/view?usp=drivesdk"><i class="bx bx-link-external"></i></a>
+          </div>
+     </div>
+
+     <div class="portfolio-box">
+          <img src="calculator.png" alt="">
+          <div class="portfolio-layer">
+             <h4>calculator</h4>
+             <p>this is the best calculator</p>
+            <a href="https://drive.google.com/file/d/1zqSCmZ9NFzeuQrhbApJBzbwkZa4pM_gX/view?usp=drivesdk"><i class="bx bx-link-external"></i></a>
+          </div>
+     </div>
+
+     <div class="portfolio-box">
+          <img src="weather.png" alt="">
+          <div class="portfolio-layer">
+             <h4>weatherapp</h4>
+             <p>this is modern weatherapp</p>
+            <a href="https://drive.google.com/file/d/1-7nAsZxAXzsu5Eqo9TkeNC7N8PbY0Tkq/view?usp=drivesdk"><i class="bx bx-link-external"></i></a>
+          </div>
+     </div>
+
+     <div class="portfolio-box">
+          <img src="hubwebsite.png" alt="">
+          <div class="portfolio-layer">
+             <h4>web design</h4>
+             <p>this is modern hubwebsite</p>
+            <a href=""><i class="bx bx-link-external"></i></a>
+          </div>
+     </div>
+</div>
+</section>
+
+<section class="contact" id="contact">
+     <h2 class="heading">contact <span>me</span></h2>
+     <div class="main-content row">
+<div class="forminput col-lg-4 col-md-4 col-12 center">
+     <form action="userinfo.php" method="post">
+     <div class="input-box">
+          <input type="text" name="name" placeholder="Full Name">
+          <input type="email" placeholder="email address" name="email">
+     </div>
+     <div class="input-box">
+          <input type="number" placeholder="mobile number" name="mobile">
+          <input type="text" placeholder="email subject" name="emailsubject">
+     </div>
+<textarea name="comment" id="" cols="30" rows="10" placeholder="your message"></textarea>
+<input type="submit" value="send message" class="btn">
+     </form>
+</div>
+
+<div class="contact2 col-lg-4 col-md-4 col-12 center">
+     <a href="" class="no-underline"><i class='bx bxs-phone-call'></i>call now> 9990147462</a>
+     <a href="" class="no-underline"><i class='bx bxs-envelope' ></i></i>email> manikak220@gmal.com</a>
+     <a href="" class="no-underline"><i class='bx bxl-whatsapp-square' ></i>whatsapp> 9990147462 </a>
+     </div>
+</div>
+</section>
+
+
+
+    <!--footer design  -->
+ <footer class="footer" id="footer">
+<div class="footer-content row">
+     <div class="row col-lg-4 col-md-4 col-12">
+          <h1>social media</h1>
+          <div class="footer-share">
+             <a href="" class="no-underline"><i class='bx bxl-facebook'></i>Facebook</a>
+               <a href="" class="no-underline"><i class='bx bxl-linkedin-square' ></i>linkedin</a>
+               <a href="" class="no-underline"><i class='bx bxl-github'></i>github</a>
+               <a href="" class="no-underline"><i class='bx bxl-instagram-alt'></i>instagram</a>
+               </div>
+     </div>
+     <div class="row col-lg-4 col-md-4 col-12">
+          <h1>more information</h1>
+          <div class="footer-share">
+          <a href="#home" class="no-underline">Home</a>
+          <a href="#about" class="no-underline">About</a>
+          <a href="#services" class="no-underline">Services</a>
+          <a href="#portfolio" class="no-underline">Portfolio</a>
+          <a href="#contact" class="no-underline">contat me</a>
+     </div>
+</div>
+ <div class="row col-lg-4 col-md-4 col-12">
+          <h1>contact me</h1>
+          <div class="footer-share">
+          <a href="#Home" class="no-underline">call-9990147462</a>
+          <a href="#Home" class="no-underline ">email-manikak220@gmail.com</a>
+          <a href="#Home" class="no-underline">whatsapp-9990147462</a>
+     </div>
+     </div>
+</div>
+<hr>
+     <div class="footer-text">
+          <p>Copyright &copy; 2023 by Manika | All Rights Reserved</p>
+     </div>
+</footer>
+<!-- scroll-behavior=scroll reveal -->
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+<script src="https://unpkg.com/scrollreveal"></script>
+<!-- typerjs -->
+</body> 
+<script>
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+   menuIcon.classList.toggle('bx-x');
+   navbar.classList.toggle('active');
+});
+
+
+// scroll section active link
+
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a');
+
+
+
+window.onscroll= ()=>{
+     sections.forEach(sec=>{
+          let top=window.scrollY;
+          let offset =sec.offsetTop-50;
+          let height= sec.offsetHeight;
+          let id=sec.getAttribute('id');
+
+          if( top >= offset && top < offset + height)  {
+               navlinks.forEach(links =>{
+links.classList.remove('active');
+document.querySelector('header nav a[href*= ' + id + ']').classList.add('active');
+               });
+
+          };
+     });
+     // scroll section active link end here
+
+     // sticky nav start from here
+
+     let header=document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+/********** remove togglr icon and navbar when click navbar link*********/
+
+
+navlinks.forEach((link) => {
+     link.addEventListener('click', () => {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+});
+  });
+
+};
+// scroll reveal
+ScrollReveal({
+      reset: true,
+      distance:'80px',
+      duration:2000,
+      delay:200
+      });
+ScrollReveal().reveal('.home-content,.heading ,.contact2', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box,.contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1,.about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+// typed js
+// const typed=new Typed('.multiple-text',{
+//      strings=['Frontened Developer','video editor','content-writter'],
+//      typeSpeed:100,
+//      backspeed:100,
+//      backDelay:1000,
+//      loop:true
+// });
+var typed = new Typed('.multiple-text', {
+  strings: ['Frontened Developer','video editor','content-writter'],
+  typeSpeed: 30
+});
+
+</script>
+</html>
